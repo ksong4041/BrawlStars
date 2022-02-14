f5d73c0090a3597b82c16b0f40ee174bb2df4169
ksong4041 <ksg4041@gmail.com>
2022-02-14 17:52:17 +0900

    BrawlStars

---
+++ New-nity/Assets/Script/LeftJoystickController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftJoystickController : MonoBehaviour
+{
+    [Header("Target")]
+    [Tooltip("움직일 대상")]
+    public Transform Character;
+
+    [Header("JoyStick")]
+    [Tooltip("움직일 대상의 움직임을 제어하는 왼쪽 스틱")]
+    [SerializeField] private RectTransform LeftFiledStick;
+
+    [Tooltip("왼쪽 조이스틱의 뒷 배경")]
+    [SerializeField] private GameObject LeftJoyPad;
+    [SerializeField] private RectTransform LeftBackBoard;
+
+    private float Radius = 0.0f;
+
+    private bool TouchCheck = false;
+
+    private float Speed = 0.0f;
+
+    private Vector2 Direction;
+
+    private Vector3 Movement;
+
+    private Vector3 Rotation;
+
+    private void Awake()
+    {
+        LeftFiledStick = GameObject.Find("LeftFiledCircle").GetComponent<RectTransform>();
+
+        LeftJoyPad = GameObject.Find("LeftOutLineCircle");
+        LeftBackBoard = LeftJoyPad.GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+        Radius = (LeftBackBoard.rect.width * 0.5f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
